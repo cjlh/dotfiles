03abc1ca15fbb454fb47f19ef637794937e94eab
Caleb Hamilton <cjlhamilton@gmail.com>
2026-05-27 16:11:20 +0100

    fix: Add missing border to which-key

--- after/plugin/whichkey.lua
+++ after/plugin/whichkey.lua
@@ -1,4 +1,13 @@
-vim.keymap.set("n", "<leader>?", function() require("which-key").show({
-    global = true
-}) end, { desc = "View global keymaps (which-key)" })
+local whichkey = require("which-key")
 
+whichkey.setup({
+    win = {
+        border = "rounded"
+    },
+})
+
+vim.keymap.set("n", "<leader>?", function()
+    whichkey.show({
+        global = true
+    })
+end, { desc = "View global keymaps (which-key)" })
