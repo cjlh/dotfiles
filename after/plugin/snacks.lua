87e3c89a41e2a4f77d4f2223b2f9fc752c2d7ba5
Caleb Hamilton <cjlhamilton@gmail.com>
2026-05-27 13:38:37 +0100

    fix: Use emmylua for formatting to avoid tab chars

--- after/plugin/snacks.lua
+++ after/plugin/snacks.lua
@@ -1,37 +1,37 @@
 require("snacks").setup({
-	image = {},
-	picker = {
-		hidden = true,
-		ignored = true,
-		sources = {
-			files = {
-				hidden = true,
-				ignored = false,
-				show_empty = true,
-			},
-			explorer = {
-				win = {
-					border = "bottom",
-					input = {
-						title = "Explorer",
-					},
-				},
-			},
-		},
-	},
-	explorer = {
-		replace_netrw = true,
-	},
+    image = {},
+    picker = {
+        hidden = true,
+        ignored = true,
+        sources = {
+            files = {
+                hidden = true,
+                ignored = false,
+                show_empty = true,
+            },
+            explorer = {
+                win = {
+                    border = "bottom",
+                    input = {
+                        title = "Explorer",
+                    },
+                },
+            },
+        },
+    },
+    explorer = {
+        replace_netrw = true,
+    },
 })
 
 vim.keymap.set("n", "<leader><leader>", function()
-	Snacks.picker.files()
+    Snacks.picker.files()
 end, { desc = "Open file picker (Snacks)" })
 
 vim.keymap.set("n", "<leader>e", function()
-	Snacks.explorer()
+    Snacks.explorer()
 end, { desc = "Open explorer (Snacks)" })
 
 vim.keymap.set("n", "<C-space>", function()
-	Snacks.picker.grep()
+    Snacks.picker.grep()
 end, { desc = "Open file search (Snacks)" })
