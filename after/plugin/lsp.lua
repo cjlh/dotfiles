c109e5e71c1ff8e1dcffe376bd89dfa14c5703a3
Caleb Hamilton <cjlhamilton@gmail.com>
2026-05-29 11:51:52 +0100

    feat: Add goto definition/declaration LSP keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -72,3 +72,6 @@ end, { desc = 'Hover documentation (LSP)' })
 vim.keymap.set('n', '<leader>F', function()
     require('conform').format()
 end, { desc = 'Format and apply autofixes (Conform)' })
+
+vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
