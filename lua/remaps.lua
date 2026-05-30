daae78bc438b04a3afedd87a78644970a67eaa01
Caleb Hamilton <cjlhamilton@gmail.com>
2026-05-30 17:06:33 +0100

    feat: Add keymap for visually selecting current word/symbol

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -46,3 +46,5 @@ end, { desc = 'Open LSP definition in new tab' })
 vim.keymap.set('i', '<C-c>', '<Esc>')
 
 vim.keymap.set('n', '<leader>s', ':%s/', { desc = 'Begin grep in file' })
+
+vim.keymap.set('n', '<leader>v', 'vin', { remap = true, desc = 'Select word/symbol under cursor' })
